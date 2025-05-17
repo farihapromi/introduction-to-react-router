845037256d6cc4a4fead57f5a27499037758efa8
Fariha Promi <farihapromi2611@gmail.com>
2025-05-17 20:10:07 +0600

    Add nested route

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
 const router=createBrowserRouter([
   {
     path:'/',
-    element:<Home></Home>
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/about',
+        element:<About/>
+      },
+      {
+        path:'/contact',
+        element:<Contact/>
+      }
+
+    ]
   }
 
 ])
