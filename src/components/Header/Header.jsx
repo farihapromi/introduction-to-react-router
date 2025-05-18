fa53234b079d2f566bd23c2962276b1898daf075
Fariha Promi <farihapromi2611@gmail.com>
2025-05-18 11:05:07 +0600

    Add navLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const baseStyle = 'p-4 rounded-lg';
+  const activeStyle = 'bg-blue-600 text-white'; // active link style
+  const inactiveStyle = 'bg-blue-400 text-black'; // default style
+
   return (
-    <div >
-        <h2>Navbar</h2>
-        <nav className='flex gap-4 ' >
-             <Link className='bg-blue-400 p-4 rounded-lg' to='/'>Home</Link>
-            <Link className='bg-blue-400 p-4 rounded-lg' to='/about'>About</Link>
-                <Link className='bg-blue-400 p-4 rounded-lg' to='/contact'>Contact</Link>
-                  <Link className='bg-blue-400 p-4 rounded-lg' to='/users'>Users</Link>
-      
- 
+    <div>
+      <h2>Navbar</h2>
+      <nav className="flex gap-4">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `${baseStyle} ${isActive ? activeStyle : inactiveStyle}`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `${baseStyle} ${isActive ? activeStyle : inactiveStyle}`
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            `${baseStyle} ${isActive ? activeStyle : inactiveStyle}`
+          }
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to="/users"
+          className={({ isActive }) =>
+            `${baseStyle} ${isActive ? activeStyle : inactiveStyle}`
+          }
+        >
+          Users
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
 
-        </nav>
-          </div>
-  )
-}
-
-export default Header
+export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
+  const navigation=useNavigation()
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">
         <h3>This is home page</h3>
-        <Outlet />
+
+        {
+          navigation.status==='loadinng'?<p>Loading.....</p>:  <Outlet />
+        }
+      
       </main>
       <Footer />
     </div>
